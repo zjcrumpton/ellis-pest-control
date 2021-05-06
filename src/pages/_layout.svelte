4e105273b406f19a91c97199e2de43d482031291
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2021-05-06 20:59:09 +0300

    add footer to layout

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,7 +1,15 @@
 <!-- routify:options preload="proximity" -->
 <script>
   import Nav from '../components/Nav/Nav.svelte'
+  import Footer from '../components/Footer/Footer.svelte'
 </script>
-
+<style>
+  .content{
+    min-height: calc(100vh - 150px);
+  }
+</style>
 <Nav />
-<slot />
+<div class="content">
+  <slot />
+</div>
+<Footer />
