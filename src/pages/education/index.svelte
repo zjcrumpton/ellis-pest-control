ca8af06d94cb9f9302ae875a55f6c54ff72f2c21
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2021-05-10 16:27:31 +0300

    add layout to education

---
+++ src/pages/education/index.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+<style>
+    div{
+        display: grid;
+        /* grid-template-columns: 30% 70%; */
+    }
+   
+</style>
+<div class="educationLayout">
+    
+    <slot />
+</div>
+
